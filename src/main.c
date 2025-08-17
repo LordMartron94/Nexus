3a650adb95ffec057f28ecc747f9e5c0209a73ef
Matthew van der Hoorn <md.registrations@protonmail.com>
2025-08-17 20:47:25 +0200

    Clean slate

--- src/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "nexus/nexus.h"
-
-int main(void) {
-    printf("[nexus] Hello from demo app!\n");
-    printf("[nexus] add(2, 3) = %d\n", nexus_add(2, 3));
-    printf("[nexus] version = %s\n", nexus_version_string());
-    return 0;
-}
